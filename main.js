25557dba69ae8b1fb6823f00400af0d15578b608
Bakhtawar Fatima <165771736+1bakhtawar@users.noreply.github.com>
2024-06-12 20:17:01 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+"use strict";
+// let number: number= 2;
+//myNum is variable
+//number is data type
+//1 is actual content/ actual value
+// let     myNum :number         //variable declare
+// myNum =  1;          //number initialize
+// let number1:number = 1;
+// number1= 10;
+// console.log(number1)
+// let weightInKg = 60;
+// let heightInMeter = 1.77;
+// let bmi = weightInKg/(heightInMeter ^ 2)
+// console.log(bmi)
+// //operators
+// //      ** = power
+// //      %   = reminder      5%2       5/2       reminder is 1
+// //Homework slide 121
+// let a : number =5;
+// let b : number =2;
+// let result : number;
+// //pre increment =  a++      //a++ = 5       //a = 6 
+// // compiler pely increment nhi keryga bad me kry ga
+// //post increment = ++a      //++a = 6 
+// //compiler pehly increment kary ga 
+// result = ++a    //a = 6
+// console.log(a--);      //5   //yahan decrement nhi hoga 
+// console.log(a); // 4   // yaha decrement hoga
+// //comparision operator
+// let budget = 20000;
+// let totalAmount = 25000;
+// //budget  barah h ya brabar  h total amount k
+// console.log(budget >= totalAmount)
+// //budget  chota h ya brabar h total amount k
+// console.log(budget <= totalAmount)
+//  // dono valaue same honi chahye
+// console.log(budget == totalAmount)
+//             // not equal
+// console.log(budget != totalAmount)
+// //Logical operators
+// let haveDegree: string = "yes";
+// let isHamzahStudent: string = "yes"
+// //  || =  ya/ or    age 1 condition bhi true hogi to ans true ayega
+// console.log(haveDegree == "yes"  || isHamzahStudent == " no ") //true
+// //   && = and    jb dono conditions true hongi to phr ans true ayega otherwise ans false ayega
+// console.log(haveDegree == "yes"  && isHamzahStudent == " no ")//false
+// //if else
+// let email = true;
+// let passWord = false;
+// if(email && passWord){
+//     console.log("Logged In")
+// }
+// else{
+//     console.log("Wrong email or passWord")
+// }
+// //switch case
+// let dayOff = "Sunday";
+// switch(dayOff){
+//     case "Saturday" :
+//         console.log("Go hiking");
+//         break;
+//     case "Sunday":
+//         console.log("Read a book...");
+//         break;
+//     default :
+//     console.log("Work on a hobby...")
+// }
+// //ternary operator
+// let isHungry : boolean = false;
+// //agr isHungry true hoga to apple print hoga or agr false h to Water print hoga
+// console.log(isHungry ? "Apple" : "Water");
+// // loops
+// let sentence = "code with me";
+// for(let i = 1; i<= 100; i++){
+//     console.log(i + " " +sentence)
+// }
+//while
+// let i = 1
+// while(i < 100){
+//     console.log(i + "  Hello")
+//     i++
+// }
+// //while loop
+// let input: boolean = true;
+// while(input==true){
+//     console.log("Hello")
+// }
+//do while loop
+// do {
+//     console.log("Hello")
+// }while(true)
---
+++ test.ts
@@ -0,0 +1,198 @@
+// // function
+
+// function printSum(){
+//     let num1 :number = 10
+//     let num2 :number= 20
+//     let result = num1  + num2
+//     console.log(result)
+// }
+
+// printSum()
+
+// function sum (Number1:number,Number2:number){
+//     let Result = Number1 + Number2
+//     console.log(Result)
+// }
+// sum(54,87)
+// sum(87,98)
+
+
+// //Arrow functions
+
+// let subtract = ()=>{
+//     let res = 9-4
+//     console.log(res)
+// }
+// subtract()
+
+
+
+// //variable scope
+// let num1 = 40;
+// function add (){
+//     let num1 = 34;
+//     console.log(num1)
+// }
+
+// add()
+// console.log(num1)
+
+
+// var num1 :number = 30;
+// var num1 :number = 30;
+// console.log(num1)
+
+
+// let num2:number=2
+// // num2=4
+// console.log(num2)
+
+// //objects
+// let person1:{
+//     name: string;
+//     fatherName: string;
+//     age :number;
+// }={
+//     name : "Ali",
+//     fatherName:"abc",
+//     age:35
+// }
+
+// console.log(person1)
+
+
+
+// //type alias
+
+// type personType= {
+//     name: string
+//     age:number
+//     gender:string
+// }
+
+// let Person1:personType = {
+//     name: "Hamzah",
+//     age:20,
+//     gender:"Male",
+
+// }
+// console.log(Person1)
+
+
+
+// //type literal
+
+// let trafficLight :"red" | "yellow" | "green"
+// trafficLight = "red" ,
+// trafficLight = "yellow",
+// trafficLight="green"
+//         // green is assign 
+// console.log(trafficLight)
+
+
+
+// //type union
+
+// let myBag :string | number
+// myBag = "Sunglasses"
+// myBag = 4
+
+// console.log(myBag)
+
+// //type intersection
+// type teacher = {
+//         name :string
+//         roll:string
+// }
+// type student = {
+//         name: string
+//         rollNum:number
+// }
+
+// let both: student & teacher ={
+//         name:"Ali",
+//         rollNum :1234,
+//         roll: "student"
+// }
+// console.log(both)
+
+
+// //Arrays
+
+// let fruit:string[] = ["Apple", "Banana" , "Orange" , "Grapes"]
+
+// // console.log(fruit[3])
+// // console.log(fruit.length)
+// console.log("original array" , fruit)
+
+// //pop method delete element from last
+
+// fruit.pop();
+// console.log("pop" , fruit)
+
+// //pop method add element in last
+
+// fruit.push("kiwi");
+// console.log("push" , fruit)
+
+// //shift method delete element from start
+
+// fruit.shift();
+// console.log("Shift" , fruit)
+
+// //unshift method add element in start
+// fruit.unshift("Strawberry")
+// console.log("unshift" , fruit);
+
+//slice method: 
+//      ye on elements ko show krta h jo hm ne aaray me diye hoty hen original array ko      change nhi krta
+
+// let fruits:string[] = ["Apple", "Banana" , "Orange" , "Grapes"]
+// console.log("original array" , fruits)
+// console.log("slice method" , fruits.slice(1,3));
+// console.log(fruits);
+
+
+// //splice : insert and delete elements and change the original array
+// let fruits:string[] = ["Apple", "Banana" , "Orange" , "Grapes"]
+// console.log("original array" , fruits)
+//            //go to  // kitny elements ko delete krna h   //add element
+// fruits.splice(2,      3, "Mango" ,                   "Pineapple")
+// console.log("after splice" , fruits);
+
+
+// // tupple: data type array me dety hain
+// let FruitsTupple:[string,number,string,string] = ["Apple", 4 , "Orange" , "Grapes"]
+// console.log(FruitsTupple);
+// // FruitsTupple.push("watermelon")   it is bad practice but kr skty hain
+// console.log(FruitsTupple);
+// console.log(FruitsTupple[3]);
+
+
+// enum colour {
+//         red,//0
+//         yellow= 4, //we can change the index number
+//         blue,//5
+//         green//6
+// }
+// console.log(colour.red)//0
+// console.log(colour.blue)//5
+// console.log(colour[2])//6
+// console.log(colour["yellow"]) //4
+
+// let trafficLight :colour = colour.blue
+// console.log(trafficLight);
+
+
+
+// //any & unknown
+// let num1 : unknown;     //provides type safety  // good practice
+// num1 = "abc";
+// num1 = 56;
+// num1 = true
+
+// let num2 : any;
+// num2 = "abc"; //bad practice
+// num2 = 56;
+// num2 = true
+
